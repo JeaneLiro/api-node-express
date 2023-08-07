61453fb5d19ee9c84c2af8bb7fa96da2e46720b0
Jeane <jeanelirosantos@gmail.com>
2023-08-07 17:58:48 -0300

    Primeira função API

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("Rodando Servidor...")
+const express = require("express")
+
+const api = express()
+
+api.get('/', (req, res) => {
+    res.json({message: "Bem-vindo a nossa API"})
+})
+
+api.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000. http://localhost:3000')
+}
+    )
